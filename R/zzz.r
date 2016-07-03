e20f9c0423bbbc130fff8b802065421530414aa0
Przemyslaw Cias <przemyslaw.cias@gmail.com>
2016-07-03 20:26:50 +0000

    changed to openstreetmap

--- R/zzz.r
+++ R/zzz.r
@@ -17,7 +17,10 @@ reverse_base <- "http://open.mapquestapi.com/nominatim/v1/reverse.php"
   packageStartupMessage("Data (c) OpenStreetMap contributors, ODbL 1.0. http://www.openstreetmap.org/copyright")
   packageStartupMessage("Nominatim Usage Policy: http://wiki.openstreetmap.org/wiki/Nominatim_usage_policy")
   packageStartupMessage("MapQuest Nominatim Terms of Use: http://info.mapquest.com/terms-of-use/")
-  options(NOMINATIM.TIMEOUT=5)
-  options(NOMINATIM.DELAY=0.5)
-  options(NOMINATIM.search_base="http://open.mapquestapi.com/nominatim/v1/search.php")
+  options(NOMINATIM.TIMEOUT=10)
+  options(NOMINATIM.DELAY=0.1)
+  #options(NOMINATIM.search_base="http://open.mapquestapi.com/nominatim/v1/search.php")
+  options(NOMINATIM.search_base="http://nominatim.openstreetmap.org/search")
+
+  
 }
